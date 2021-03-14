00b609ae39d31b8d029a5a3074cb761f926e62a8
JPieterwas <jakub.pieterwas@microsoft.wsei.edu.pl>
2021-03-14 09:31:52 +0100

    Funkcjonalnosc powitalna

---
+++ EmeryturaInteraktywnie/Emerytura/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Emerytura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj!");
+
+            Console.Write("Podaj imie:");
+            string imie;
+            imie=Console.ReadLine();
+
+            Console.Write("Podaj nazwisko:");
+            string nazwisko;
+            nazwisko=Console.ReadLine();
+            Console.WriteLine("Witaj " +" "+ imie +" "+ nazwisko + " !");
+            Console.WriteLine("Witaj {0} {1}!", imie, nazwisko);
+            Console.WriteLine("Witaj {imie} {nazwisko}!");
+
+        }
+    }
+}
